dadd99b688fabb587d1053e3a83b4400b6841d9d
Ahmed <ahmedshaaban97>
2019-01-06 21:50:38 +0200

    bookapp

---
+++ src/component/welcome.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Welcome = ()=>{
+  return (
+      <div>
+          hi my man
+      </div>
+  );
+};
+
+export default Welcome;
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+        {title : 'Harry potter'},
+        {title : 'dark knight'},
+        {title : 'Avengers'},
+        {title : 'Justic League'},
+    ]
+}
